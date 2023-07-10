91364ac5b0cc5b2a91548906cc66870df4948afc
Firuza Aliyeva <firuza.aliyeva@procure.az>
2023-07-10 10:32:26 +0400

    JVMNAME used on a function

--- mylibrary/src/main/java/com/fsa/mylibrary/Main.kt
+++ mylibrary/src/main/java/com/fsa/mylibrary/Main.kt
@@ -2,6 +2,7 @@ package com.fsa.mylibrary
 
 public class Main {
     public companion object {
+        @JvmName("myHiddenFunction")
         internal fun returnsMeow(): String = "Meow"
         public fun returnsSmth(): String = returnsMeow()
     }
